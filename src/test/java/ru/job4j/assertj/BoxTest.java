9b39c158b26ad69fe2aa89cd715d6530dce1d609
Arifich80 <arifich@yahoo.com>
2023-03-28 14:27:19 +0300

    Утверждения с примитивными типами [#504883]

---
+++ src/test/java/ru/job4j/assertj/BoxTest.java
@@ -0,0 +1,71 @@
+package ru.job4j.assertj;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BoxTest {
+    @Test
+    void isThisSphere() {
+        Box box = new Box(0, 10);
+        String name = box.whatsThis();
+        assertThat(name).isEqualTo("Sphere")
+                .isNotBlank();
+    }
+
+    @Test
+    void isThisCube() {
+        Box box = new Box(8, 12);
+        String name = box.whatsThis();
+        assertThat(name).isEqualTo("Cube")
+                .isNotNull();
+    }
+
+    @Test
+    void isExist() {
+        Box box = new Box(0, 10);
+        boolean result = box.isExist();
+        assertThat(result).isTrue()
+                .isNotNull();
+    }
+
+    @Test
+    void notExist() {
+        Box box = new Box(0, -1);
+        boolean result = box.isExist();
+        assertThat(result).isFalse()
+                .isNotNull();
+    }
+
+    @Test
+    void NumberOfVertices() {
+        Box box = new Box(8, 12);
+        int result = box.getNumberOfVertices();
+        assertThat(result).isEqualTo(8)
+                .isNotNull();
+    }
+
+    @Test
+    void WrongNumberOfVertices() {
+        Box box = new Box(10, 12);
+        int result = box.getNumberOfVertices();
+        assertThat(result).isNotEqualTo(10)
+                .isNotNull();
+    }
+
+    @Test
+    void CubeArea() {
+        Box box = new Box(10, 12);
+        double result = box.getArea();
+        assertThat(result).isEqualTo(0.0)
+                .isNotNull();
+    }
+
+    @Test
+    void TetrahedronArea() {
+        Box box = new Box(4, 0);
+        double result = box.getArea();
+        assertThat(result).isEqualTo(0.0)
+                .isNotNull();
+    }
+}
